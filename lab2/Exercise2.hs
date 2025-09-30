0d9c00b92aaa4df88d88ba43f0c220a20cd440cc
Jasper Wink <117076930+JasperWink@users.noreply.github.com>
2025-09-30 18:49:28 +0200

    Better tests

--- lab2/Exercise2.hs
+++ lab2/Exercise2.hs
@@ -1,14 +1,22 @@
 
--- Time spend:
+-- Time spend: x min
 
--- Test length
--- Check if the intersection of two non-interceting sets are empty
--- Check if the resulting set is in the powerset of the intersection
+
 
 import Data.List
 import System.Random
 import Test.QuickCheck
 import SetOrd
+
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+
+instance (Ord a, Arbitrary a) => Arbitrary (Set a) where
+  arbitrary = do
+    xs <- arbitrary
+    return (list2set xs)
 
 
 setIntersection :: Ord a => Set a -> Set a -> Set a
@@ -32,16 +40,46 @@ setDifference set1 (Set []) = set1
 setDifference set1 (Set (y:ys)) = setDifference (deleteSet y set1) (Set ys)
 
 
-setLength :: Set a -> Int a
-setLength (Set xs) = length xs
+-- Test symetry
+prop_IntersectionSym :: Set Int -> Set Int -> Bool
+prop_IntersectionSym set1 set2 =
+    setIntersection set1 set2 == setIntersection set2 set1
+
+prop_UnionSym :: Set Int -> Set Int -> Bool
+prop_UnionSym set1 set2 =
+    setUnion set1 set2 == setUnion set2 set1
+
+prop_DifferenceSym :: Set Int -> Set Int -> Bool
+prop_DifferenceSym set1 set2 =
+    set1 /= set2 --> setDifference set1 set2 /= setDifference set2 set1
 
 
--- testLength :: Set a -> Set a -> Bool a
--- testLength set1 set2 = setLength $ setIntercection set1 set2 <= setLength set1
---                     && setLength $ setIntercection set1 set2 <= setLength set2
---                     && setLength <= setUnion set1 set2 
+-- Subset tests
+prop_IntersectionSubset :: Set Int -> Set Int -> Bool
+prop_IntersectionSubset set1 set2 =
+    subSet (setIntersection set1 set2) set1 && subSet (setIntersection set1 set2) set2
+
+prop_UnionSubset :: Set Int -> Set Int -> Bool
+prop_UnionSubset set1 set2 =
+    subSet set1 (setUnion set1 set2) && subSet set2 (setUnion set1 set2)
+
+prop_DifferenceSubset :: Set Int -> Set Int -> Bool
+prop_DifferenceSubset set1 set2 =
+    subSet (setDifference set1 set2) set1
 
 
+-- Test empty
+prop_IntersectionEmpty :: Set Int -> Bool
+prop_IntersectionEmpty set1 =
+    isEmpty $ setIntersection emptySet set1
+
+prop_UnionEmpty :: Set Int -> Bool
+prop_UnionEmpty set1 =
+    setUnion emptySet set1 == set1
+
+prop_DifferenceEmpty :: Set Int -> Bool
+prop_DifferenceEmpty set1 =
+    isEmpty $ setDifference emptySet set1
 
 
 main :: IO ()
@@ -49,14 +87,18 @@ main = do
     let set1 = list2set [1,2,3]
     let set2 = list2set [2,3,4]
 
-    print $ setIntersection set1 set2
-    print $ setUnion set1 set2
-    print $ setDifference set1 set2
+    print $ setIntersection set1 set2 == list2set([2,3])
+    print $ setUnion set1 set2 == list2set([1,2,3,4])
+    print $ setDifference set1 set2 == list2set([1])
 
-    -- testLength set1 set2
+    quickCheck prop_IntersectionEmpty
+    quickCheck prop_IntersectionSym
+    quickCheck prop_IntersectionSubset
 
-    isEmpty setIntersection set1 set2 -> isEmpty set1 || isEmpty set2
-    isEmpty setUnion set1 set2 -> isEmpty set1 && isEmpty set2
-    isEmpty setDifference set1 set2 -> isEmpty set1
+    quickCheck prop_UnionEmpty
+    quickCheck prop_UnionSym
+    quickCheck prop_UnionSubset
 
-
+    quickCheck prop_DifferenceEmpty
+    quickCheck prop_DifferenceSym
+    quickCheck prop_DifferenceSubset
