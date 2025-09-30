8f49bf51b7ae35da909bdb5c7fd99da6c57bfc07
GMvdDonck <donckg@casema.nl>
2025-09-30 22:01:18 +0200

    Add a property and some description

--- lab2/Exercise4.hs
+++ lab2/Exercise4.hs
@@ -1,11 +1,17 @@
 -- TODO: Increment timer if you're picking up work on this file
 -- Time spent: 120 min
 
-{-
+{- Test report:
 
+R = {(x, y) | x ≡ y(mod n)}, n > 0.
+To be serial, x and y must be in the domain, meaning y(mod n) must also be in the domain.
+It could be tested by verifying that in the relation, every x of a domain has a relation to a y where any mod (> 0) of y is x.
+To prove that R is serial, 
 -}
 
 module Exercise4 where
+
+import Test.QuickCheck
 
 -- List of pairs
 type Rel a = [(a,a)]
@@ -28,3 +34,16 @@ domainHasPair domain a rel = any (pairInDomain domain) (pairsByFirstElem a rel)
 -- Checks if relation is serial on a given domain
 isSerial :: Eq a => [a] -> Rel a -> Bool
 isSerial domain rel = all (\a -> domainHasPair domain a rel) domain
+
+
+-- Property 1: in the relation, every element of the domain relates to another element of the domain
+prop_allToAny :: Rel a -> Bool
+prop_allToAny (r:rs) = False -- TODO: this is basically done in the main code so how to quick test that it does?
+
+
+-- Property 2:
+
+
+main :: IO ()
+main = do
+    quickCheck prop_allToAny
