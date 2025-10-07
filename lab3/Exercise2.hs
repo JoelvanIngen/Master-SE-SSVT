87efaede9ddb1a8a4d51e513eea0a5ec3d868d50
Jasper Wink <117076930+JasperWink@users.noreply.github.com>
2025-10-07 21:33:41 +0200

    Added mutator to countSurvivers as input. Added checks after 2

--- lab3/Exercise2.hs
+++ lab3/Exercise2.hs
@@ -7,38 +7,19 @@ import MultiplicationTable
 import Control.Monad
 
 
--- Properties
--- prop_tenElements
--- prop_firstElementIsInput
--- prop_sumIsTriangleNumberTimesInput
--- prop_linear
--- prop_moduloIsZero
-
--- Mutations
--- anyList
--- removeElements
--- addElements
-
-
-multiplicationTableProps2 :: [[Integer] -> Integer -> Bool]
--- multiplicationTableProps2 = [prop_tenElements, prop_firstElementIsInput, prop_sumIsTriangleNumberTimesInput, prop_linear, prop_moduloIsZero]
-multiplicationTableProps2 = [prop_tenElements, prop_sumIsTriangleNumberTimesInput, prop_linear, prop_moduloIsZero]
-
-
--- mutators = [anyList, removeElements, addElements]
-mutators2 = [anyList, removeElements, addElements]
-
-
-
-countSurvivors :: Integer -> [[Integer] -> Integer -> Bool] -> (Integer -> [Integer]) -> IO Integer
-countSurvivors n props fut = do
+countSurvivors :: Integer
+                  -> [[Integer] -> Integer -> Bool]
+                  -> [[Integer] -> Gen [Integer]]
+                  -> (Integer -> [Integer])
+                  -> IO Integer
+countSurvivors n props muts fut = do
     results <- replicateM (fromIntegral n) $ generate $ do
         -- Check if the mutant survives by using the input properties and mutations.
-        mutator <- elements mutators
+        mutator <- elements muts
         input <- arbitrary :: Gen Integer
-        propResults <- mutate' mutator props fut input
+        mutateResult <- mutate' mutator props fut input
         -- Ignore if the mutate returns an empty list.
-        case propResults of
+        case mutateResult of
             [] -> return False
             results -> return (all (== True) results)
     -- Return the amout of mutants that survived.
@@ -47,5 +28,48 @@ countSurvivors n props fut = do
 
 main :: IO ()
 main = do
-    survivors <- countSurvivors 4000 multiplicationTableProps2 multiplicationTable
+    -- 139 survivors
+    survivors <- countSurvivors 4000 [prop_tenElements] [anyList] multiplicationTable
+    print survivors
+
+    -- survivors <- countSurvivors 4000 [prop_firstElementIsInput] [anyList] multiplicationTable
+    -- print survivors
+
+    survivors <- countSurvivors 4000 [prop_sumIsTriangleNumberTimesInput] [anyList] multiplicationTable
+    print survivors
+
+    survivors <- countSurvivors 4000 [prop_linear] [anyList] multiplicationTable
+    print survivors
+
+    survivors <- countSurvivors 4000 [prop_moduloIsZero] [anyList] multiplicationTable
+    print survivors
+
+    survivors <- countSurvivors 4000 [prop_tenElements] [removeElements] multiplicationTable
+    print survivors
+
+    -- survivors <- countSurvivors 4000 [prop_firstElementIsInput] [removeElements] multiplicationTable
+    -- print survivors
+
+    survivors <- countSurvivors 4000 [prop_sumIsTriangleNumberTimesInput] [removeElements] multiplicationTable
+    print survivors
+
+    survivors <- countSurvivors 4000 [prop_linear] [removeElements] multiplicationTable
+    print survivors
+
+    survivors <- countSurvivors 4000 [prop_moduloIsZero] [removeElements] multiplicationTable
+    print survivors
+
+    survivors <- countSurvivors 4000 [prop_tenElements] [addElements] multiplicationTable
+    print survivors
+
+    -- survivors <- countSurvivors 4000 [prop_firstElementIsInput] [addElements] multiplicationTable
+    -- print survivors
+
+    survivors <- countSurvivors 4000 [prop_sumIsTriangleNumberTimesInput] [addElements] multiplicationTable
+    print survivors
+
+    survivors <- countSurvivors 4000 [prop_linear] [addElements] multiplicationTable
+    print survivors
+
+    survivors <- countSurvivors 4000 [prop_moduloIsZero] [addElements] multiplicationTable
     print survivors
--- lab3/Exercise3.hs
+++ lab3/Exercise3.hs
@@ -13,9 +13,9 @@ findSubsetsWithMinSurvivors xs = map snd $ filter ((== minfst) . fst) xs
     where minfst = minimum (map fst xs)
 
 
-findMinimumLengthSubsets :: [[Int]] -> [[Int]]
-findMinimumLengthSubsets [] = []
-findMinimumLengthSubsets xs =
+findMinimalLengthSubsets :: [[Int]] -> [[Int]]
+findMinimalLengthSubsets [] = []
+findMinimalLengthSubsets xs =
     let minLength = minimum (map length xs)
     in filter (\subset -> length subset == minLength) xs
 
@@ -25,13 +25,13 @@ findMinimalPropertySubset props fut = do
     let indexSubsets = tail $ subsequences [0..length props - 1]
         propSubsets = map (\indices -> map (props !!) indices) indexSubsets
 
-    survivorsPerSubset <- mapM (\propSubset -> countSurvivors 1000 propSubset fut) propSubsets
+    survivorsPerSubset <- mapM (\propSubset -> countSurvivors 4000 propSubset fut) propSubsets
 
     let zippedSurvivorsAndIndexes = zip survivorsPerSubset indexSubsets
         subsetsWithMinSurvivors = findSubsetsWithMinSurvivors zippedSurvivorsAndIndexes
-        minimumLengthSubsets = findMinimumLengthSubsets subsetsWithMinSurvivors
+        -- minimumLengthSubsets = findMinimalLengthSubsets subsetsWithMinSurvivors
 
-    return minimumLengthSubsets
+    return subsetsWithMinSurvivors
 
 
 main :: IO ()
