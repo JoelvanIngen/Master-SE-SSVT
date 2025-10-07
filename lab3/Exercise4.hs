0ede11d94db3236518346732128b21f8653182ae
GMvdDonck <donckg@casema.nl>
2025-10-07 22:59:33 +0200

    Update type declaration

--- lab3/Exercise4.hs
+++ lab3/Exercise4.hs
@@ -9,18 +9,19 @@ If using a subset from exercise 3, the results are...
 module Exercise4 where
 
 import Data.List
+import Test.QuickCheck
 import Mutation
 import MultiplicationTable
 import Control.Monad
 import Exercise2 (countSurvivors)
 
-strength :: Integer -> [[Integer] -> Integer -> Bool] -> (Integer -> [Integer]) -> IO Float
-strength n props fut = do
-    x <- countSurvivors n props fut
+strength :: Integer -> [[Integer] -> Integer -> Bool] -> [[Integer] -> Gen [Integer]] -> (Integer -> [Integer]) -> IO Float
+strength n props muts fut = do
+    x <- countSurvivors n props muts fut
     return (((fromIntegral n - fromIntegral x) / fromIntegral n) * 100)
 
 
 main :: IO ()
 main = do
-    z <- strength 1000 multiplicationTableProps multiplicationTable
+    z <- strength 1000 multiplicationTableProps mutators multiplicationTable
     print z
