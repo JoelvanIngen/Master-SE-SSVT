acbfefc0572beee324c6594dddb361ab621729b6
Jasper Wink <117076930+JasperWink@users.noreply.github.com>
2025-10-07 22:00:22 +0200

    Added Exercise2 test report

--- lab3/Exercise2.hs
+++ lab3/Exercise2.hs
@@ -1,4 +1,22 @@
--- Time spent: x min
+-- Time spent: 240 min
+{-
+Test report:
+
+The countSurvivor function takes properties and mutators as input and checks how many
+    mutations are not detected by the properties.
+
+According to the properties the prop_tenElements is the best property because
+    it lets the lowesrt amount of mutants survive, it does mis a few with the anyList mutator
+    because those list can be 10 long by chance.
+
+Besides that the removeElement mutator seems to be the hardest mutator to detect.
+    two property checks even let all mutants survive.
+
+But if these properties are combined when all mutators are active they catch all mutants.
+    therefor the mutation tests are correct.
+-}
+
+
 module Exercise2 where
 import Data.List
 import Test.QuickCheck
@@ -14,7 +32,7 @@ countSurvivors :: Integer
                   -> IO Integer
 countSurvivors n props muts fut = do
     results <- replicateM (fromIntegral n) $ generate $ do
-        -- Check if the mutant survives by using the input properties and mutations.
+        -- Check if the mutant survives by using the test properties.
         mutator <- elements muts
         input <- arbitrary :: Gen Integer
         mutateResult <- mutate' mutator props fut input
@@ -28,48 +46,69 @@ countSurvivors n props muts fut = do
 
 main :: IO ()
 main = do
-    -- 139 survivors
+    print "----- All mutators and properties -----"
+    -- 0 survivors
+    survivors <- countSurvivors 4000 multiplicationTableProps mutators multiplicationTable
+    print survivors
+
+    print "----- Any List mutator -----"
+    -- ~140 survivors
     survivors <- countSurvivors 4000 [prop_tenElements] [anyList] multiplicationTable
     print survivors
 
     -- survivors <- countSurvivors 4000 [prop_firstElementIsInput] [anyList] multiplicationTable
     -- print survivors
 
+    -- ~3 survivors
     survivors <- countSurvivors 4000 [prop_sumIsTriangleNumberTimesInput] [anyList] multiplicationTable
     print survivors
 
+    -- ~270 survivors
     survivors <- countSurvivors 4000 [prop_linear] [anyList] multiplicationTable
     print survivors
 
+    -- ~320 survivors
     survivors <- countSurvivors 4000 [prop_moduloIsZero] [anyList] multiplicationTable
     print survivors
 
+    print "----- Remove Element mutator -----"
+    -- 0 survivors
     survivors <- countSurvivors 4000 [prop_tenElements] [removeElements] multiplicationTable
     print survivors
 
+    --  survivors
     -- survivors <- countSurvivors 4000 [prop_firstElementIsInput] [removeElements] multiplicationTable
     -- print survivors
 
+    -- ~60 survivors
     survivors <- countSurvivors 4000 [prop_sumIsTriangleNumberTimesInput] [removeElements] multiplicationTable
     print survivors
 
+    -- 4000 survivors
     survivors <- countSurvivors 4000 [prop_linear] [removeElements] multiplicationTable
     print survivors
 
+    -- 4000 survivors
     survivors <- countSurvivors 4000 [prop_moduloIsZero] [removeElements] multiplicationTable
     print survivors
 
+    print "----- Add Element mutator -----"
+    -- 0 survivors
     survivors <- countSurvivors 4000 [prop_tenElements] [addElements] multiplicationTable
     print survivors
 
-    -- survivors <- countSurvivors 4000 [prop_firstElementIsInput] [addElements] multiplicationTable
-    -- print survivors
+    -- ~200 survivors
+    survivors <- countSurvivors 4000 [prop_firstElementIsInput] [addElements] multiplicationTable
+    print survivors
 
+    -- ~20 survivors
     survivors <- countSurvivors 4000 [prop_sumIsTriangleNumberTimesInput] [addElements] multiplicationTable
     print survivors
 
+    -- 0 survivors
     survivors <- countSurvivors 4000 [prop_linear] [addElements] multiplicationTable
     print survivors
 
+    -- ~200 survivors
     survivors <- countSurvivors 4000 [prop_moduloIsZero] [addElements] multiplicationTable
     print survivors
